a2a47693a86f6a14a038d19c67c92c781d406f98
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-05-12 16:36:45 +0100

    Add seed data

---
+++ seeders/20220512152644-vehicles.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.bulkInsert('Vehicles', [{
+     make: 'YAMAHA',
+     model: 'VMX1700 V-MAX',
+     createdAt: new Date(),
+     updatedAt: new Date()
+   },
+   {
+    make: 'UD',
+    model: '1800',
+    createdAt: new Date(),
+    updatedAt: new Date()
+   },
+   {
+    make: 'BMW',
+    model: 'HP2 Sport',
+    createdAt: new Date(),
+    updatedAt: new Date()
+   }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Vehicles', null, {});
+  }
+};
